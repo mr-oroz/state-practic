68a849f9fc7a2694587f91ff0e1d525d59fb6094
mr-oroz <oroz7307@gmail.comm>
2021-08-09 14:32:46 +0600

    add prevState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import State1 from "./components/State";
-
+import List from "./components/List/List";
+const posts = [
+    {label: 'Privet kak dela', important: false, id: 1},
+    {label: 'Privet kak tvoi dela', important: true, id: 2},
+    {label: 'Privet chem dela', important: true, id: 3},
+]
 function App() {
+    const elements = posts.map((item, id) => {
+        return (
+            <List key={id} {...item}/>
+        )
+    })
   return (
       <>
-          <State1/>
+          {/*<State1 />*/}
+          {elements}
       </>
   );
 }
